d4aec5cacc105ae887a7da28811f988035b817ac
JianWind <15142063060@163.com>
2018-08-24 14:16:23 +0800

    更新DTO与Usercontroller

--- src/main/java/com/sk/boot/springboot/web/UserController.java
+++ src/main/java/com/sk/boot/springboot/web/UserController.java
@@ -1,66 +1,71 @@
 package com.sk.boot.springboot.web;
 
+import java.util.Date;
+
+import com.sk.boot.springboot.DTO.ExecuteDTO;
+import com.sk.boot.springboot.DTO.PageQueryParamDTO;
+import com.sk.boot.springboot.DTO.PageResultDTO;
 import com.sk.boot.springboot.entity.User;
-import com.sk.boot.springboot.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import com.sk.boot.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.util.List;
-
-@Controller
+/**
+ * 会员控制器
+ *
+ * @author 刘冬 博客出处：http://www.cnblogs.com/GoodHelper/
+ *
+ */
+@RestController
+@RequestMapping("member")
 public class UserController {
 
-    @Resource
-    UserService userService;
+    @Autowired
+    private UserRepository repository;
 
-
-    @RequestMapping("/")
-    public String index() {
-        return "redirect:/index";
+    @GetMapping("get/{id}")
+    public User get(@PathVariable Long id) {
+        return repository.findById(id);
     }
 
-    @RequestMapping("/index")
-    public String index(Model model) {
-        return "user/index";
+    @PostMapping("save")
+    public ExecuteDTO save(@RequestBody User entity) {
+        repository.save(entity);
+        return new ExecuteDTO(true, "保存成功", entity.getId());
     }
 
-    @RequestMapping("/list")
-    public String list(Model model) {
-        List<User> users=userService.getUserList();
-        model.addAttribute("users", users);
-        return "user/list";
+    @PostMapping("loadPage")
+    public PageResultDTO loadPage(@RequestBody PageQueryParamDTO params) {
+
+        // 动态查询条件
+        Specification<User> spec = (root, query, cb) -> {
+            if (params.getQuery() != null) {
+                // 筛选 会员姓名
+                query.where(cb.equal(root.get("name"), params.getQuery().toString()));
+            }
+
+            return null;
+        };
+        Pageable pageable = new PageRequest(params.getPage() - 1, params.getSize());
+        Page<User> pageResult = repository.findAll( pageable);
+
+        // 返回分页数据
+        return new PageResultDTO(pageResult.getTotalElements(), pageResult.getContent());
     }
 
-    @RequestMapping("/toAdd")
-    public String toAdd() {
-        return "user/userAdd";
+    @GetMapping("remove/{id}")
+    public ExecuteDTO remove(@PathVariable Long id) {
+        repository.delete(id);
+        return new ExecuteDTO(true, "删除成功", id);
     }
 
-    @RequestMapping("/add")
-    public String add(User user) {
-        userService.save(user);
-        return "redirect:/list";
-    }
-
-    @RequestMapping("/toEdit")
-    public String toEdit(Model model,String username) {
-        User user=userService.findUserByUsername(username);
-        model.addAttribute("user", user);
-        return "user/userEdit";
-    }
-
-    @RequestMapping("/edit")
-    public String edit(User user) {
-        userService.edit(user);
-        return "redirect:/list";
-    }
-
-
-    @RequestMapping("/delete")
-    public String delete(Long id) {
-        userService.delete(id);
-        return "redirect:/list";
-    }
 }
